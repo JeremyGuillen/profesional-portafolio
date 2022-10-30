6c1a10be34076f7139768fc0ff3d64f24479d7c3
jeremyguillen <jeremy.guillen@lantern.tech>
2022-10-30 10:25:13 -0600

    added header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,18 @@ const Home: NextPage = () => {
     <div className="flex min-h-screen flex-col">
       <main>
         <Navbar />
-        <div className="bg-[url('/wave-background.svg')] bg-no-repeat bg-cover h-screen">
-          <p>Hola mundo</p>
-        </div>
+        <section className="bg-center bg-[url('/wave-background.svg')] h-screen bg-no-repeat bg-cover md:h-[80vh] relative px-11">
+          <div className="absolute top-1/3 -translate-y-1/2">
+            <h1 className="text-2xl md:text-4xl text-white tracking-wide">
+              Hello, my <br />
+              name is
+              <span className="font-medium"> Jeremy</span>
+            </h1>
+            <p className="pt-6 text-lg md:text-2xl text-white font-light">
+              I&apos;m a software engineer with <br /> passion for innovation
+            </p>
+          </div>
+        </section>
       </main>
     </div>
   );
