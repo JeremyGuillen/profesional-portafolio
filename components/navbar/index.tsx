747ac2141d4c8f32072de516d81ba8c9f4d47468
jeremyguillen <jeremy.guillen@lantern.tech>
2022-10-30 21:20:24 -0600

    started services section

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,25 +3,25 @@ import React from 'react';
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className=" sticky px-4 top-0 left-0 w-full flex justify-evenly items-center bg-primary text-white z-50">
+    <nav className="sticky px-4 top-0 left-0 w-full flex justify-evenly items-center bg-primary text-white z-50">
       <div className="flex-grow-0">
         <img src="/white-logo.png" alt="Logo" className="h-36" />
       </div>
       <ul className="list-none flex-grow flex items-center justify-end text-white">
-        <li className="after:transition-all after:-translate-x-1/2 px-2 relative after:content-[''] after:absolute after:bg-white after:bottom-1 after:w-0 after:h-0 after:left-1/2 hover:after:h-px hover:after:w-3/4 cursor-pointer">
-          <a>Home</a>
+        <li className=" after:transition-all after:-translate-x-1/2 px-2 relative after:content-[''] after:absolute after:bg-white after:bottom-1 after:w-0 after:h-0 after:left-1/2 hover:after:h-px hover:after:w-3/4 cursor-pointer">
+          <a className="text-white hover:text-white">Home</a>
         </li>
         <li className="after:transition-all after:-translate-x-1/2 px-2 relative after:content-[''] after:absolute after:bg-white after:bottom-1 after:w-0 after:h-0 after:left-1/2 hover:after:h-px hover:after:w-3/4 cursor-pointer">
-          <a>About</a>
+          <a className="text-white hover:text-white">About</a>
         </li>
         <li className="after:transition-all after:-translate-x-1/2 px-2 relative after:content-[''] after:absolute after:bg-white after:bottom-1 after:w-0 after:h-0 after:left-1/2 hover:after:h-px hover:after:w-3/4 cursor-pointer">
-          <a>Services</a>
+          <a className="text-white hover:text-white">Services</a>
         </li>
         <li className="after:transition-all after:-translate-x-1/2 px-2 relative after:content-[''] after:absolute after:bg-white after:bottom-1 after:w-0 after:h-0 after:left-1/2 hover:after:h-px hover:after:w-3/4 cursor-pointer">
-          <a>Projects</a>
+          <a className="text-white hover:text-white">Projects</a>
         </li>
         <li className="after:transition-all after:-translate-x-1/2 px-2 relative after:content-[''] after:absolute after:bg-white after:bottom-1 after:w-0 after:h-0 after:left-1/2 hover:after:h-px hover:after:w-3/4 cursor-pointer">
-          <a>Contact</a>
+          <a className="text-white hover:text-white">Contact</a>
         </li>
       </ul>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import type { NextPage } from 'next';
 import { Navbar } from '../components/navbar';
+import { ServiceCard } from '../components/service-card';
+import { DesktopOutlined, MobileOutlined, CloudServerOutlined } from '@ant-design/icons';
+
+const frontEndTools = ['Angular', 'React', 'Tailwind', 'SCSS', 'Motion'];
+const mobileDevelopmentTools = ['React Native', 'Flutter'];
+const fullstackDevelopmentToold = ['Angular, React, NodeJS, AWS'];
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +22,24 @@ const Home: NextPage = () => {
             <p className="pt-6 text-lg md:text-2xl text-white font-light">
               I&apos;m a software engineer with <br /> passion for innovation
             </p>
+          </div>
+        </section>
+        {/* About me section */}
+        <section className="py-6 h-96 bg-white"></section>
+
+        {/* Services section */}
+        <section className="py-9 bg-primary px-4">
+          <h3 className="text-center text-white">Services</h3>
+          <div className="flex justify-center">
+            <div className="flex-1 px-4 max-w-[400px] h-[459px]">
+              <ServiceCard title="Front end development" iconSrc={<DesktopOutlined />} description="I'll bring your ideas and designs to life and deliver you a clean and scalable project." tools={frontEndTools} />
+            </div>
+            <div className="flex-1 px-4 max-w-[400px] h-[459px]">
+              <ServiceCard title="Mobile development" iconSrc={<MobileOutlined />} description="I'll develop and bring your mobile applications to the market in both android and ios." tools={mobileDevelopmentTools} />
+            </div>
+            <div className="flex-1 px-4 max-w-[400px] h-[459px]">
+              <ServiceCard title="Full stack development" iconSrc={<CloudServerOutlined />} description="I'll connect your creative ideas with a system that will solve your necesities." tools={fullstackDevelopmentToold} />
+            </div>
           </div>
         </section>
       </main>
