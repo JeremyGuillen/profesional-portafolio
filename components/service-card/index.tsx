f2026e1f0a14c8d7838a9de982b61d2653c8f61b
Jeremy <guillenjeremy26@gmail.com>
2022-11-02 20:12:37 -0500

    finished services section

--- components/service-card/index.tsx
+++ components/service-card/index.tsx
@@ -13,28 +13,30 @@ interface ServiceCardProps {
 
 export const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, iconSrc, tools }) => {
   return (
-    <Card style={{ width: '100%', borderRadius: 10, maxWidth: '400px', height: '100%' }}>
-      <div className="flex justify-center">
-        <div className={classNames(css.hexagon, 'flex', 'justify-center', 'items-center')}>
-          <span className="text-3xl text-white">{iconSrc}</span>
+    <Card style={{ width: '100%', borderRadius: 10, maxWidth: '400px', height: '100%' }} bodyStyle={{ height: '100%' }}>
+      <div className="flex flex-col h-full">
+        <div className="flex justify-center">
+          <div className={classNames(css.hexagon, 'flex', 'justify-center', 'items-center')}>
+            <span className="text-3xl text-white">{iconSrc}</span>
+          </div>
         </div>
-      </div>
-      <h3 className="pt-3 text-center text-secondary font-medium text-lg">{title}</h3>
-      <p className="text-center mb-7">{description}</p>
-      <h5 className="text-secondary text-center mb-2">Tools</h5>
-      <List
-        dataSource={tools}
-        className="h-32 mb-4"
-        renderItem={(tool) => (
-          <p className="text-center mb-1" key={tool}>
-            {tool}
-          </p>
-        )}
-      />
-      <div className="text-center mt-2">
-        <Button type="primary" shape="round" style={{ backgroundColor: '#4EADFE', borderColor: '#4EADFE' }}>
-          Contact me
-        </Button>
+        <h3 className="pt-3 text-center text-secondary font-medium text-lg">{title}</h3>
+        <p className="text-center mb-7 text-base">{description}</p>
+        <h5 className="text-secondary text-center mb-2">Tools</h5>
+        <List
+          dataSource={tools}
+          className="h-32 mb-4"
+          renderItem={(tool) => (
+            <p className="text-center mb-1 text-base" key={tool}>
+              {tool}
+            </p>
+          )}
+        />
+        <div className="text-center mt-auto">
+          <Button type="primary" shape="round" style={{ backgroundColor: '#4EADFE', borderColor: '#4EADFE' }}>
+            Contact me
+          </Button>
+        </div>
       </div>
     </Card>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { DesktopOutlined, MobileOutlined, CloudServerOutlined } from '@ant-desig
 
 const frontEndTools = ['Angular', 'React', 'Tailwind', 'SCSS', 'Motion'];
 const mobileDevelopmentTools = ['React Native', 'Flutter'];
-const fullstackDevelopmentToold = ['Angular, React, NodeJS, AWS'];
+const fullstackDevelopmentToold = ['Angular', 'React', 'NodeJS', 'AWS'];
 
 const Home: NextPage = () => {
   return (
@@ -29,15 +29,17 @@ const Home: NextPage = () => {
 
         {/* Services section */}
         <section className="py-9 bg-primary px-4">
-          <h3 className="text-center text-white">Services</h3>
-          <div className="flex justify-center">
-            <div className="flex-1 px-4 max-w-[400px] h-[459px]">
+          <h3 className="text-center text-white text-2xl pb-5">
+            <span className="relative after:transition-all after:-translate-x-1/2 after:content-[''] after:absolute after:bg-white after:bottom-0 after:w-2/3 after:h-[2px] after:left-1/2">Services</span>
+          </h3>
+          <div className="flex justify-center flex-wrap">
+            <div className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
               <ServiceCard title="Front end development" iconSrc={<DesktopOutlined />} description="I'll bring your ideas and designs to life and deliver you a clean and scalable project." tools={frontEndTools} />
             </div>
-            <div className="flex-1 px-4 max-w-[400px] h-[459px]">
+            <div className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
               <ServiceCard title="Mobile development" iconSrc={<MobileOutlined />} description="I'll develop and bring your mobile applications to the market in both android and ios." tools={mobileDevelopmentTools} />
             </div>
-            <div className="flex-1 px-4 max-w-[400px] h-[459px]">
+            <div className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
               <ServiceCard title="Full stack development" iconSrc={<CloudServerOutlined />} description="I'll connect your creative ideas with a system that will solve your necesities." tools={fullstackDevelopmentToold} />
             </div>
           </div>
