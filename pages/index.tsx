171efd5e3be00430979ad6cd7b7e300ba4f3601e
jeremyguillen26 <jeremy.guillen@linkam.com>
2022-11-28 07:59:29 -0600

    adding variants and animations to the services card section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from 'next';
 import { useEffect, useRef } from 'react';
-import { motion, useAnimation, useInView, Variants } from 'framer-motion';
+import { motion, useAnimationControls, useInView, Variants } from 'framer-motion';
 import { Button } from 'antd';
 import { DesktopOutlined, MobileOutlined, CloudServerOutlined, GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
 import { Navbar } from '../components/navbar';
@@ -16,23 +16,59 @@ const fadeInAnimation: Variants = {
   hidden: {
     opacity: 0,
     translateX: -200,
+    transition: { duration: 1 },
   },
   visible: {
     opacity: 1,
     translateX: 0,
+    transition: { duration: 1 },
+  },
+};
+
+const cardsInAnimation: Variants = {
+  hidden: {
+    opacity: 0,
+    translateY: 50,
+  },
+  visible: {
+    opacity: 1,
+    translateY: 0,
+    animationTimingFunction: 'ease-in',
   },
 };
 
 const Home: NextPage = () => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  const servicesRef = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.5 });
+  const areServicesInView = useInView(servicesRef, { once: true, amount: 0.5 });
+  const controls = useAnimationControls();
+  const itemsControls = useAnimationControls();
   const onSocialLinkClicked = (url: string) => {
     window.open(url, '_blank');
   };
 
+  const cardAnimationSequence = async () => {
+    if (areServicesInView) {
+      itemsControls.start((i) => ({ ...cardsInAnimation.visible, transition: { duration: 1, delay: i * 0.2 } }));
+    } else {
+      itemsControls.start(cardsInAnimation.hidden);
+    }
+  };
+
+  useEffect(() => {
+    cardAnimationSequence();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [areServicesInView]);
+
   useEffect(() => {
     console.log('Element is in view: ', isInView);
-  }, [isInView]);
+    if (isInView) {
+      controls.start('visible');
+    } else {
+      controls.start('hidden');
+    }
+  }, [isInView, controls]);
 
   return (
     <div className="flex min-h-screen flex-col">
@@ -54,13 +90,7 @@ const Home: NextPage = () => {
         </section>
         {/* About me section */}
         <section className="pt-16 pb-40 bg-white" ref={ref}>
-          <motion.div
-            className="flex items-center w-4/5 m-auto max-w-5xl"
-            style={{
-              transform: isInView ? 'none' : 'translateX(-200)',
-              opacity: isInView ? 1 : 0,
-            }}
-          >
+          <motion.div animate={controls} variants={fadeInAnimation} className="flex items-center w-4/5 m-auto max-w-5xl">
             <div className="flex-1 pr-7">
               <div className="flex items-center">
                 <h4 className="text-xl m-0 pr-2 text-primary font-normal">About me</h4>
@@ -86,16 +116,16 @@ const Home: NextPage = () => {
           <h3 className="text-center text-white text-2xl pb-5">
             <span className="relative after:transition-all after:-translate-x-1/2 after:content-[''] after:absolute after:bg-white after:bottom-0 after:w-2/3 after:h-[2px] after:left-1/2">Services</span>
           </h3>
-          <div className="flex justify-center flex-wrap">
-            <div className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
+          <div ref={servicesRef} className="flex justify-center flex-wrap">
+            <motion.div custom={1} animate={itemsControls} className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
               <ServiceCard title="Front end development" iconSrc={<DesktopOutlined />} description="I'll bring your ideas and designs to life and deliver you a clean and scalable project." tools={frontEndTools} />
-            </div>
-            <div className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
+            </motion.div>
+            <motion.div custom={2} animate={itemsControls} className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
               <ServiceCard title="Mobile development" iconSrc={<MobileOutlined />} description="I'll develop and bring your mobile applications to the market in both android and ios." tools={mobileDevelopmentTools} />
-            </div>
-            <div className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
+            </motion.div>
+            <motion.div custom={3} animate={itemsControls} className="flex-1 px-4 max-w-[400px] h-[500px] min-w-[377px] pt-3">
               <ServiceCard title="Full stack development" iconSrc={<CloudServerOutlined />} description="I'll connect your creative ideas with a system that will solve your necesities." tools={fullstackDevelopmentToold} />
-            </div>
+            </motion.div>
           </div>
         </section>
 
